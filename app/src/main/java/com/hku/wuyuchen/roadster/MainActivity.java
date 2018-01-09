7090a7af130a38af225160b7dbe7a6e8a3d36d10
wuyuchen <wuyuchen@bytedance.com>
2018-01-09 13:45:47 +0800

    feat: init project

---
+++ app/src/main/java/com/hku/wuyuchen/roadster/MainActivity.java
@@ -0,0 +1,26 @@
+package com.hku.wuyuchen.roadster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    static {
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "OpenCV not loaded");
+        } else {
+            Log.d(TAG, "OpenCV loaded！");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
