4074a28a8ac3947eea11cbf0e29bff4ec3c7a91e
wuyuchen <wuyuchen@bytedance.com>
2018-01-09 15:43:06 +0800

    feat: opencv demo

--- app/src/main/java/com/hku/wuyuchen/roadster/MainActivity.java
+++ app/src/main/java/com/hku/wuyuchen/roadster/MainActivity.java
@@ -1,26 +1,257 @@
 package com.hku.wuyuchen.roadster;
 
-import android.support.v7.app.AppCompatActivity;
+import java.util.Arrays;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import android.R.string;
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-import org.opencv.android.OpenCVLoader;
+public class MainActivity extends Activity implements CvCameraViewListener2{
+    private String TAG = "OpenCV_Test";
+    //OpenCV的相机接口
+    private CameraBridgeViewBase mCVCamera;
+    //缓存相机每帧输入的数据
+    private Mat mRgba,mTmp;
+    //按钮组件
+    private Button mButton;
+    //当前处理状态
+    private static int Cur_State = 0;
 
-public class MainActivity extends AppCompatActivity {
+    private Size mSize0;
+    private Mat mIntermediateMat;
+    private MatOfInt mChannels[];
+    private MatOfInt mHistSize;
+    private int mHistSizeNum = 25;
+    private Mat mMat0;
+    private float[] mBuff;
+    private MatOfFloat mRanges;
+    private Point mP1;
+    private Point mP2;
+    private Scalar mColorsRGB[];
+    private Scalar mColorsHue[];
+    private Scalar mWhilte;
+    private Mat mSepiaKernel;
 
-    private static final String TAG = "MainActivity";
-
-    static {
-        if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "OpenCV not loaded");
-        } else {
-            Log.d(TAG, "OpenCV loaded！");
+    /**
+     * 通过OpenCV管理Android服务，异步初始化OpenCV
+     */
+    BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status){
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG,"OpenCV loaded successfully");
+                    mCVCamera.enableView();
+                    break;
+                default:
+                    break;
+            }
         }
-    }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCVCamera = (CameraBridgeViewBase) findViewById(R.id.camera_view);
+        mCVCamera.setCvCameraViewListener(this);
+
+        mButton = (Button) findViewById(R.id.deal_btn);
+        mButton.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(Cur_State<8){
+                    //切换状态
+                    Cur_State ++;
+                }else{
+                    //恢复初始状态
+                    Cur_State = 0;
+                }
+            }
+
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG,"OpenCV library not found!");
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mCVCamera!=null){
+            mCVCamera.disableView();
+        }
+    };
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        // TODO Auto-generated method stub
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mTmp = new Mat(height, width, CvType.CV_8UC4);
+
+        mIntermediateMat = new Mat();
+        mSize0 = new Size();
+        mChannels = new MatOfInt[] { new MatOfInt(0), new MatOfInt(1), new MatOfInt(2) };
+        mBuff = new float[mHistSizeNum];
+        mHistSize = new MatOfInt(mHistSizeNum);
+        mRanges = new MatOfFloat(0f, 256f);
+        mMat0 = new Mat();
+        mColorsRGB = new Scalar[] { new Scalar(200, 0, 0, 255), new Scalar(0, 200, 0, 255), new Scalar(0, 0, 200, 255) };
+        mColorsHue = new Scalar[] {
+                new Scalar(255, 0, 0, 255), new Scalar(255, 60, 0, 255), new Scalar(255, 120, 0, 255), new Scalar(255, 180, 0, 255), new Scalar(255, 240, 0, 255),
+                new Scalar(215, 213, 0, 255), new Scalar(150, 255, 0, 255), new Scalar(85, 255, 0, 255), new Scalar(20, 255, 0, 255), new Scalar(0, 255, 30, 255),
+                new Scalar(0, 255, 85, 255), new Scalar(0, 255, 150, 255), new Scalar(0, 255, 215, 255), new Scalar(0, 234, 255, 255), new Scalar(0, 170, 255, 255),
+                new Scalar(0, 120, 255, 255), new Scalar(0, 60, 255, 255), new Scalar(0, 0, 255, 255), new Scalar(64, 0, 255, 255), new Scalar(120, 0, 255, 255),
+                new Scalar(180, 0, 255, 255), new Scalar(255, 0, 255, 255), new Scalar(255, 0, 215, 255), new Scalar(255, 0, 85, 255), new Scalar(255, 0, 0, 255)
+        };
+        mWhilte = Scalar.all(255);
+        mP1 = new Point();
+        mP2 = new Point();
+
+        // Fill sepia kernel
+        mSepiaKernel = new Mat(4, 4, CvType.CV_32F);
+        mSepiaKernel.put(0, 0, /* R */0.189f, 0.769f, 0.393f, 0f);
+        mSepiaKernel.put(1, 0, /* G */0.168f, 0.686f, 0.349f, 0f);
+        mSepiaKernel.put(2, 0, /* B */0.131f, 0.534f, 0.272f, 0f);
+        mSepiaKernel.put(3, 0, /* A */0.000f, 0.000f, 0.000f, 1f);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        // TODO Auto-generated method stub
+        mRgba.release();
+        mTmp.release();
+    }
+
+    /**
+     * 图像处理都写在此处
+     */
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        Size sizeRgba = mRgba.size();
+        int rows = (int) sizeRgba.height;
+        int cols = (int) sizeRgba.width;
+        Mat rgbaInnerWindow;
+
+        int left = cols / 8;
+        int top = rows / 8;
+
+        int width = cols * 3 / 4;
+        int height = rows * 3 / 4;
+
+        switch (Cur_State) {
+            case 1:
+                //灰化处理
+                Imgproc.cvtColor(inputFrame.gray(), mRgba, Imgproc.COLOR_GRAY2RGBA,4);
+                break;
+            case 2:
+                //Canny边缘检测
+                mRgba = inputFrame.rgba();
+                Imgproc.Canny(inputFrame.gray(), mTmp, 80, 100);
+                Imgproc.cvtColor(mTmp, mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
+                break;
+            case 3:
+                //Hist直方图计算
+                Mat hist = new Mat();
+                int thikness = (int) (sizeRgba.width / (mHistSizeNum + 10) / 5);
+                if(thikness > 5) thikness = 5;
+                int offset = (int) ((sizeRgba.width - (5*mHistSizeNum + 4*10)*thikness)/2);
+
+                // RGB
+                for(int c=0; c<3; c++) {
+                    Imgproc.calcHist(Arrays.asList(mRgba), mChannels[c], mMat0, hist, mHistSize, mRanges);
+                    Core.normalize(hist, hist, sizeRgba.height/2, 0, Core.NORM_INF);
+                    hist.get(0, 0, mBuff);
+                    for(int h=0; h<mHistSizeNum; h++) {
+                        mP1.x = mP2.x = offset + (c * (mHistSizeNum + 10) + h) * thikness;
+                        mP1.y = sizeRgba.height-1;
+                        mP2.y = mP1.y - 2 - (int)mBuff[h];
+                        Imgproc.line(mRgba, mP1, mP2, mColorsRGB[c], thikness);
+                    }
+                }
+                // Value and Hue
+                Imgproc.cvtColor(mRgba, mTmp, Imgproc.COLOR_RGB2HSV_FULL);
+                // Value
+                Imgproc.calcHist(Arrays.asList(mTmp), mChannels[2], mMat0, hist, mHistSize, mRanges);
+                Core.normalize(hist, hist, sizeRgba.height/2, 0, Core.NORM_INF);
+                hist.get(0, 0, mBuff);
+                for(int h=0; h<mHistSizeNum; h++) {
+                    mP1.x = mP2.x = offset + (3 * (mHistSizeNum + 10) + h) * thikness;
+                    mP1.y = sizeRgba.height-1;
+                    mP2.y = mP1.y - 2 - (int)mBuff[h];
+                    Imgproc.line(mRgba, mP1, mP2, mWhilte, thikness);
+                }
+                break;
+            case 4:
+                //Sobel边缘检测
+                Mat gray = inputFrame.gray();
+                Mat grayInnerWindow = gray.submat(top, top + height, left, left + width);
+                rgbaInnerWindow = mRgba.submat(top, top + height, left, left + width);
+                Imgproc.Sobel(grayInnerWindow, mIntermediateMat, CvType.CV_8U, 1, 1);
+                Core.convertScaleAbs(mIntermediateMat, mIntermediateMat, 10, 0);
+                Imgproc.cvtColor(mIntermediateMat, rgbaInnerWindow, Imgproc.COLOR_GRAY2BGRA, 4);
+                grayInnerWindow.release();
+                rgbaInnerWindow.release();
+                break;
+            case 5:
+                //SEPIA(色调变换)
+                rgbaInnerWindow = mRgba.submat(top, top + height, left, left + width);
+                Core.transform(rgbaInnerWindow, rgbaInnerWindow, mSepiaKernel);
+                rgbaInnerWindow.release();
+                break;
+            case 6:
+                //ZOOM放大镜
+                Mat zoomCorner = mRgba.submat(0, rows / 2 - rows / 10, 0, cols / 2 - cols / 10);
+                Mat mZoomWindow = mRgba.submat(rows / 2 - 9 * rows / 100, rows / 2 + 9 * rows / 100, cols / 2 - 9 * cols / 100, cols / 2 + 9 * cols / 100);
+                Imgproc.resize(mZoomWindow, zoomCorner, zoomCorner.size());
+                Size wsize = mZoomWindow.size();
+                Imgproc.rectangle(mZoomWindow, new Point(1, 1), new Point(wsize.width - 2, wsize.height - 2), new Scalar(255, 0, 0, 255), 2);
+                zoomCorner.release();
+                mZoomWindow.release();
+                break;
+            case 7:
+                //PIXELIZE像素化
+                rgbaInnerWindow = mRgba.submat(top, top + height, left, left + width);
+                Imgproc.resize(rgbaInnerWindow, mIntermediateMat, mSize0, 0.1, 0.1, Imgproc.INTER_NEAREST);
+                Imgproc.resize(mIntermediateMat, rgbaInnerWindow, rgbaInnerWindow.size(), 0., 0., Imgproc.INTER_NEAREST);
+                rgbaInnerWindow.release();
+                break;
+            default:
+                //显示原图
+                mRgba = inputFrame.rgba();
+                break;
+        }
+        //返回处理后的结果数据
+        return mRgba;
     }
 }
